7649466159399cbee04fa9f7ff5c0502e03f5df6
gpetz <gpetz@users.noreply.github.com>
2023-12-06 17:15:46 +0000

    Deploying to gh-pages from @ ONB-RD/cws@b7c5d23b280817d1e1d145fecd4f337a06cf7264 🚀

---
+++ _next/static/chunks/144.bbfc8a50bacc2a62.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[144],{2144:function(t){t.exports=JSON.parse('{"collection":"https://iiif.onb.ac.at/presentation/collection/lustspiel/","featured":["http://iiif.onb.ac.at/presentation/ABO/Z102981103/manifest/"],"metadata":["Author","Place of Publication"]}')}}]);
