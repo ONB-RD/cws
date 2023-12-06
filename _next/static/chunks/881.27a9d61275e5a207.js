d25d9a2166e39094ab2d3faa31a329cba1132ea8
gpetz <gpetz@users.noreply.github.com>
2023-12-06 17:40:28 +0000

    Deploying to gh-pages from @ ONB-RD/cws@84d20454679be2ab020abd48d808e1ad41c90646 🚀

--- _next/static/chunks/881.27a9d61275e5a207.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[881],{8881:function(t){t.exports=JSON.parse('{"collection":"https://digitalcollections.lib.washington.edu/iiif/2/korean/manifest.json","featured":["http://iiif.onb.ac.at/presentation/ABO/Z103519907/manifest/"],"metadata":["Author","Place of Publication"]}')}}]);
